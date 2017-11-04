31c408bd4396b70ff08c1a4137b4e130e1d9285e
kalyan <kalyankris92@gmail.com>
2017-11-04 15:40:29 +0530

    custom observable

--- observables-practise/src/app/home/home.component.ts
+++ observables-practise/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/Rx';
+import { Observer } from 'rxjs/Observer';
 
 @Component({
   selector: 'app-home',
@@ -12,15 +13,33 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    const numbers = Observable.interval(1000);
-    numbers.subscribe(
-      (number : number) => {
-        console.log(number);
-        
-      }
+    // const numbers = Observable.interval(1000);
+    // numbers.subscribe(
+    //   (number : number) => {
+    //     console.log(number);     
+    //   }
+    // );
+    const  myobserver = Observable.create((observer : Observer<string>) => {
+      setTimeout(() => {
+        observer.next('first package');
+      },2000);
+      setTimeout(() => {
+        observer.next('second package');
+      },4000);
+      setTimeout(() => {
+       // observer.error('fail package');
+      },5000);
+      setTimeout(() => {
+        observer.complete();
+      },5000);
+    });
+    myobserver.subscribe(
+      (data:string) => {console.log(data);},
+      (error:string) => {console.log(error);},
+      () => {console.log('completed');}
     );
-  }
-
+  } 
+ 
 
 
 
