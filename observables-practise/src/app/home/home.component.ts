73f4c37b5d434f2fe10ce836d5a63437dc0a91e7
kalyan <kalyankris92@gmail.com>
2017-11-04 14:59:09 +0530

    simple observable example

--- observables-practise/src/app/home/home.component.ts
+++ observables-practise/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/Rx';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +12,16 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const numbers = Observable.interval(1000);
+    numbers.subscribe(
+      (number : number) => {
+        console.log(number);
+        
+      }
+    );
   }
+
+
+
 
 }
