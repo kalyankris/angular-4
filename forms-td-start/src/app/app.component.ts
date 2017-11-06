1efa5abd70dae37a0cd67542142a4ff7c95a39b7
Kalyan Kumar Rajuri <kalyan.k@SONATA.LOCAL>
2017-11-06 15:53:00 +0530

    show form details on submit

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -10,7 +10,16 @@ export class AppComponent {
   @ViewChild('f') singup : NgForm;
   defaultQuestion = 'pet';
   answer ='';
-  genders =['male','female'] 
+  genders =['male','female'] ;
+  user = {
+    username:'',
+    email:'',
+    secretoption:'',
+    answer:'',
+    gender:''
+  };
+  submitted = false;
+
   suggestUserName() {
     const suggestedName = 'Superuser';
     // this.singup.setValue({
@@ -32,7 +41,12 @@ export class AppComponent {
   //   console.log(form);    
   // }
   onSubmit(){
-    console.log(this.singup);    
+    this.submitted = true;
+    this.user.username = this.singup.value.userData.username;
+    this.user.email = this.singup.value.userData.email;
+    this.user.secretoption = this.singup.value.secretoption;
+    this.user.answer = this.singup.value.questionAnswer;
+    this.user.gender = this.singup.value.gender;
   }
   
 }
