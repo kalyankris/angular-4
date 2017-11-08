913bc13454471f74b4f765ab57d2abdd544dcaf7
Kalyan Kumar Rajuri <kalyan.k@SONATA.LOCAL>
2017-11-08 15:41:05 +0530

    Reactive: Creating Custom Validators

--- forms-reactive-start/src/app/app.component.ts
+++ forms-reactive-start/src/app/app.component.ts
@@ -1,30 +1,43 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup,FormControl, Validators, FormArray } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   genders = ['male', 'female'];
-  signUpForm : FormGroup;
+  signupForm: FormGroup;
+  forbiddenUsernames = ['Chris', 'Anna'];
 
-  ngOnInit(){
-    this.signUpForm = new FormGroup({
+  constructor() {}
+
+  ngOnInit() {
+    this.signupForm = new FormGroup({
       'userData': new FormGroup({
-        'username': new FormControl(null, Validators.required),
-        'email': new FormControl(null, [Validators.required,Validators.email])
-      }),      
-      'gender': new FormControl('male', Validators.required),
-      'hobbies':new FormArray([])
+        'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
+        'email': new FormControl(null, [Validators.required, Validators.email])
+      }),
+      'gender': new FormControl('male'),
+      'hobbies': new FormArray([])
     });
+    // this.signupForm.valueChanges.subscribe(
+    //   (value) => console.log(value)
+    // );
   }
-  onSubmit(){
-    console.log(this.signUpForm);    
+
+  onAddHobby() {
+    const control = new FormControl(null, Validators.required);
+    (<FormArray>this.signupForm.get('hobbies')).push(control);
   }
-  onAddHobby(){
-    const control = new FormControl(null,Validators.required);
-    (<FormArray>this.signUpForm.get('hobbies')).push(control);
+
+  forbiddenNames(control: FormControl): {[s: string]: boolean} {
+    if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
+      return {'nameIsForbidden': true};
+    }
+    return null;
   }
+
 }
