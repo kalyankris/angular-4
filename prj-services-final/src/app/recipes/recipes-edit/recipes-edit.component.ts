a90c34541ab992ee57ca74d136c3bcd94185a972
kalyan <kalyankris92@gmail.com>
2017-11-18 17:01:20 +0530

    Adding new Ingredient Controls

--- prj-services-final/src/app/recipes/recipes-edit/recipes-edit.component.ts
+++ prj-services-final/src/app/recipes/recipes-edit/recipes-edit.component.ts
@@ -60,4 +60,13 @@ recipeForm : FormGroup;
     
   }
 
+  onAddIngredient(){
+   (<FormArray>this.recipeForm.get('ingredients')).push(
+     new FormGroup({
+      'name': new FormControl(),
+      'amount': new FormControl()
+     })
+   );
+  }
+
 }
