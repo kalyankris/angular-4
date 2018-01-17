e73733abf9432267869949d9e7b2d6835e170808
kalyan <kalyankris92@gmail.com>
2018-01-18 00:03:40 +0530

    Restructuring Services to use the Child Injector,moving services to core module

--- prj-services-final/src/app/app.module.ts
+++ prj-services-final/src/app/app.module.ts
@@ -1,17 +1,11 @@
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
-import { AuthGuard } from './auth/auth-guard.service';
-import { AuthSerice } from './auth/auth.service';
-import { DataStorageService } from './shared/data-storage.service';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { ShoppingListService } from './shopping-list/shopping-list.service';
-import { RecipeService } from 'app/recipes/recipe.service';
 import { ShoppingListModule } from 'app/shopping-list/shopping-list.module';
 import { AuthModule } from 'app/auth/auth.module';
 
@@ -28,7 +22,6 @@ import { AuthModule } from 'app/auth/auth.module';
     AuthModule,
     CoreModule
   ],
-  providers: [ShoppingListService,RecipeService,DataStorageService,AuthSerice,AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- prj-services-final/src/app/core/core.module.ts
+++ prj-services-final/src/app/core/core.module.ts
@@ -1,3 +1,7 @@
+import { AuthSerice } from './../auth/auth.service';
+import { DataStorageService } from './../shared/data-storage.service';
+import { RecipeService } from './../recipes/recipe.service';
+import { ShoppingListService } from './../shopping-list/shopping-list.service';
 import { SharedModule } from './../shared/shared.module';
 import { NgModule } from '@angular/core';
 import { HeaderComponent } from 'app/core/header/header.component';
@@ -16,6 +20,12 @@ import { AppRoutingModule } from 'app/app-routing.module';
   exports: [
     AppRoutingModule,
     HeaderComponent
+  ],
+  providers: [
+    ShoppingListService,
+    RecipeService,
+    DataStorageService,
+    AuthSerice,
   ]
 })
 export class CoreModule{
