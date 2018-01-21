c2269bc9055b24c1a3f0d3f95415da2d097d0de0
kalyan <kalyankris92@gmail.com>
2018-01-21 16:00:43 +0530

    Modifying Requests in Interceptors - to send token to the requests

--- angular 5/src/app/shared/auth.interceptor.ts
+++ angular 5/src/app/shared/auth.interceptor.ts
@@ -1,9 +1,15 @@
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
+import { Injectable } from '@angular/core';
+import { AuthService } from 'app/auth/auth.service';
 
+@Injectable()
 export class AuthInterceptor implements HttpInterceptor {
+  constructor(private authSerice: AuthService) {}
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    // const copiedReq =   req.clone({headers: req.headers.set('', '')});
+    const copiedReq =   req.clone({params: req.params.set('auth', this.authSerice.getToken())});
     console.log('Intercepted!', req);
-    return next.handle(req);
+    return next.handle(copiedReq);
   }
 }
